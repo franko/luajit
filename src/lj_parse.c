7a7d4dd353027fe18405f7b6174e05ed359fad0c
Francesco Abbate <francesco.bbt@gmail.com>
2022-04-01 10:09:05 +0200

    Enable short function syntax extension

--- src/lj_parse.c
+++ src/lj_parse.c
@@ -1840,14 +1840,15 @@ static void expr_table(LexState *ls, ExpDesc *e)
 }
 
 /* Parse function parameters. */
-static BCReg parse_params(LexState *ls, int needself)
+static BCReg parse_params_ext(LexState *ls, int needself,
+                              int start_token, int end_token)
 {
   FuncState *fs = ls->fs;
   BCReg nparams = 0;
-  lex_check(ls, '(');
+  lex_check(ls, start_token);
   if (needself)
     var_new_lit(ls, nparams++, "self");
-  if (ls->token != ')') {
+  if (ls->token != end_token) {
     do {
       if (ls->token == TK_name || (!LJ_52 && ls->token == TK_goto)) {
 	var_new(ls, nparams++, lex_str(ls));
@@ -1863,8 +1864,13 @@ static BCReg parse_params(LexState *ls, int needself)
   var_add(ls, nparams);
   lua_assert(fs->nactvar == nparams);
   bcreg_reserve(fs, nparams);
-  lex_check(ls, ')');
+  lex_check(ls, end_token);
   return nparams;
+}
+
+static BCReg parse_params(LexState *ls, int needself)
+{
+  return parse_params_ext(ls, needself, '(', ')');
 }
 
 /* Forward declaration. */
@@ -1902,6 +1908,50 @@ static void parse_body(LexState *ls, ExpDesc *e, int needself, BCLine line)
   }
   lj_lex_next(ls);
 }
+
+#ifdef USE_SHORT_FN_SYNTAX
+static void parse_simple_body (LexState *ls, ExpDesc *fne, int line) {
+  FuncState fs, *pfs = ls->fs;
+  FuncScope bl;
+  BCReg kidx;
+  BCLine lastline;
+  GCproto *pt;
+  ptrdiff_t oldbase = pfs->bcbase - ls->bcstack;
+  BCIns ins;
+  ExpDesc e;
+
+  fs_init(ls, &fs);
+  fscope_begin(&fs, &bl, 0);
+  fs.linedefined = line;
+  fs.numparams = (uint8_t)parse_params_ext(ls, 0, '|', '|');
+  fs.bcbase = pfs->bcbase + pfs->pc;
+  fs.bclim = pfs->bclim - pfs->pc;
+  bcemit_AD(&fs, BC_FUNCF, 0, 0);  /* Placeholder. */
+
+  fs.flags |= PROTO_HAS_RETURN;
+  expr(ls, &e);
+  ins = BCINS_AD(BC_RET1, expr_toanyreg(&fs, &e), 2);
+  if (fs.flags & PROTO_CHILD)
+    bcemit_AJ(&fs, BC_UCLO, 0, 0);  /* May need to close upvalues first. */
+  bcemit_INS(&fs, ins);
+
+  lastline = ls->linenumber;
+  pt = fs_finish(ls, lastline);
+  pfs->bcbase = ls->bcstack + oldbase;  /* May have been reallocated. */
+  pfs->bclim = (BCPos)(ls->sizebcstack - oldbase);
+  /* Store new prototype in the constant array of the parent. */
+  kidx = const_gc(pfs, obj2gco(pt), LJ_TPROTO);
+  expr_init(fne, VRELOCABLE, bcemit_AD(pfs, BC_FNEW, 0, kidx));
+#if LJ_HASFFI
+  pfs->flags |= (fs.flags & PROTO_FFI);
+#endif
+  if (!(pfs->flags & PROTO_CHILD)) {
+    if (pfs->flags & PROTO_HAS_RETURN)
+      pfs->flags |= PROTO_FIXUP_RETURN;
+    pfs->flags |= PROTO_CHILD;
+  }
+}
+#endif
 
 /* Parse expression list. Last expression is left open. */
 static BCReg expr_list(LexState *ls, ExpDesc *v)
@@ -2040,6 +2090,12 @@ static void expr_simple(LexState *ls, ExpDesc *v)
     lj_lex_next(ls);
     parse_body(ls, v, 0, ls->linenumber);
     return;
+#ifdef USE_SHORT_FN_SYNTAX
+    case '|': {
+      parse_simple_body(ls, v, ls->linenumber);
+      return;
+    }
+#endif
   default:
     expr_primary(ls, v);
     return;
